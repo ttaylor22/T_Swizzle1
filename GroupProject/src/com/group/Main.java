a88ac7fe9583274d4bf6ce68530ea201105d2889
alana.icenroad <alana.icenroad@2UA2181JXF.DRMAIN.local>
2019-09-06 14:48:17 -0400

    Update

--- GroupProject/src/com/group/Main.java
+++ GroupProject/src/com/group/Main.java
@@ -2,35 +2,39 @@ package com.group;
 
 public class Main {
 	
-	public static void mike() {
+	public static void mike() 
+	{
 		System.out.println("Mike Ramrirez");
 	}
 	
-	public static void alex() {
+	public static void alex() 
+	{
 		System.out.println("Mike Likes Feet.");
 	}
   
-  public static void heinrich()
+	public static void heinrich()
 	{
 		System.out.println("Heinrich Chen");
 	}
 	
 	public static void alana()
 	{
-		System.out.println("This line is from Alana Icenoad");
-  }
+		System.out.println("Alana Icenoad");
+	}
 	
-  public static void jlugo() {
+	public static void jlugo() 
+	{
 		System.out.println("Johnathan Lugo");
 	}
+	
 	public static void main(String [] args){
 	
 		mike();
 		alex();
-    heinrich();
-    alana();
-    jlugo();
-    MikeIsTrash.playsWoW();
+		heinrich();
+		alana();
+		jlugo();
+		MikeIsTrash.playsWoW();
 		MikeIsTrash.activates();
 		MikeIsTrash.eatsMashPotatoes();
 	}
