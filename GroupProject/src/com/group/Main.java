b286e544e8c6533feef493d6796124eb82cffc05
Alex Larcheveque <larchevequealex@gmail.com>
2019-09-06 14:18:28 -0400

    Added Alex() Method

--- GroupProject/src/com/group/Main.java
+++ GroupProject/src/com/group/Main.java
@@ -6,8 +6,14 @@ public class Main {
 		System.out.println("Mike Ramrirez");
 	}
 	
+	public static void alex() {
+		System.out.println("Mike Likes Feet.");
+	}
+	
 	public static void main(String [] args){
 		Main m = new Main();
 		m.mike();
 	}
+	
+	
 }
