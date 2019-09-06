7047ad5279b286b8b96d3d1820b498237b632220
alana.icenroad <alana.icenroad@2UA2181JXF.DRMAIN.local>
2019-09-06 14:18:33 -0400

    Commit from Alana

--- GroupProject/src/com/group/Main.java
+++ GroupProject/src/com/group/Main.java
@@ -7,7 +7,12 @@ public class Main {
 	}
 	
 	public static void main(String [] args){
-		Main m = new Main();
-		m.mike();
+		mike();
+		alana();
+	}
+	
+	public static void alana()
+	{
+		System.out.println("This line is from Alana Icenoad");
 	}
 }
