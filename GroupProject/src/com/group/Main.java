83dd8a3eb4971350499042ed3f78e38d73372374
mikeeR23 <41097168+mikeeR23@users.noreply.github.com>
2019-09-06 14:26:58 -0400

    Update Main.java

--- GroupProject/src/com/group/Main.java
+++ GroupProject/src/com/group/Main.java
@@ -21,11 +21,11 @@ public class Main {
   }
 	
 	public static void main(String [] args){
-		Main m = new Main();
-		m.mike();
-		m.alex();
-    m.heinrich();
-    m.alana();
+	
+		mike();
+		alex();
+    heinrich();
+    alana();
 	}
 	
 	
