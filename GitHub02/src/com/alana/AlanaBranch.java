2da1fe6d13d76e2aabc9a2f0d7d76edcec2e0263
alana.icenroad <alana.icenroad@2UA2181JXF.DRMAIN.local>
2019-09-06 14:09:05 -0400

    Commit from Alana

---
+++ GitHub02/src/com/alana/AlanaBranch.java
@@ -0,0 +1,14 @@
+package com.alana;
+
+public class AlanaBranch 
+{
+	public static void main (String [] args)
+	{
+		alana();
+	}
+	
+	public static void alana()
+	{
+		System.out.println("This line is from Alana's branch");
+	}
+}
