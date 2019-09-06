f4e9a8418036eddbe618a2dcbd8927e71de28972
Alex Larcheveque <larchevequealex@gmail.com>
2019-09-06 14:42:26 -0400

    Tried this on Intellij you scrubs.

--- GroupProject/src/com/group/Main.java
+++ GroupProject/src/com/group/Main.java
@@ -1,38 +1,37 @@
 package com.group;
 
 public class Main {
-	
+
 	public static void mike() {
 		System.out.println("Mike Ramrirez");
 	}
-	
+
 	public static void alex() {
 		System.out.println("Mike Likes Feet.");
 	}
-  
-  public static void heinrich()
-	{
+
+	public static void heinrich() {
 		System.out.println("Heinrich Chen");
 	}
-	
-	public static void alana()
-	{
+
+	public static void alana() {
 		System.out.println("This line is from Alana Icenoad");
-  }
-	
-  public static void jlugo() {
+	}
+
+	public static void jlugo() {
 		System.out.println("Johnathan Lugo");
 	}
-	public static void main(String [] args){
-	
+
+	public static void main(String[] args) {
+
 		mike();
 		alex();
-    heinrich();
-    alana();
-    jlugo();
-    MikeIsTrash.playsWoW();
+		heinrich();
+		alana();
+		jlugo();
+		MikeIsTrash.playsWoW();
 		MikeIsTrash.activates();
 		MikeIsTrash.eatsMashPotatoes();
 	}
-	
+}
 	
