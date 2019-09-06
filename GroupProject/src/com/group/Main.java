d2de5e02a1d5c347f164a549273b43bd1e4a72bb
michael.ramirez <michael.ramirez@MXL3251CGL.DRMAIN.local>
2019-09-06 14:02:50 -0400

    initial commit

---
+++ GroupProject/src/com/group/Main.java
@@ -0,0 +1,13 @@
+package com.group;
+
+public class Main {
+	
+	public static void mike() {
+		System.out.println("Mike Ramrirez");
+	}
+	
+	public static void main(String [] args){
+		Main m = new Main();
+		m.mike();
+	}
+}
